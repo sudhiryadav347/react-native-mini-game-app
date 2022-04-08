da7cd9be9bd9fd5b9713817078464b10b5924a19
Sudhir <sudhir@livelgam.com>
2022-04-08 22:11:06 +0530

    add image background

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
 
 
 export default function App() {
   return (
-      <LinearGradient colors={['#e69e1a', '#811b29']} style={styles.rootScreen}>
+    <LinearGradient colors={['#e69e1a', '#811b29']} style={styles.rootScreen}>
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode="cover"
+        style={styles.rootScreen} 
+        imageStyle={styles.backgroundImage}
+      >
         <StartGameScreen />
-      </LinearGradient>
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1
+  },
+  backgroundImage: {
+    opacity: 0.15
   }
 });
